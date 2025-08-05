48fa59f97df8ee95eec9a509e946463c4a58f3d1
PedroCalazansTecnol <pedro.calazans@sistemastecnol.com.br>
2025-08-05 14:44:52 -0300

    feat(carro): adiciona verificação para retorno nulo na consulta por ID

--- app/Http/Controllers/CarroController.php
+++ app/Http/Controllers/CarroController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Carro;
+
+use function PHPUnit\Framework\isString;
+
 class carroController extends Controller
 {
     public function consultaID($id)
     {
-        //REALIZA A BUSCA NO BANCO COM ID QUE FOI RECEBIDO
-        
-        return response()->json(Carro::find($id));
+        $carro = Carro::find($id);
+
+        if(empty($carro)){
+            return response()->json(['erro' => 'Carro não encontrado'], 404);
+        }
+        return response()->json($carro);
     }
 }
